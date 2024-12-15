467cf2076580dca14bbbc9d782c6594692c4c298
Marco Peyfuss <mpeyfuss@gmail.com>
2024-12-15 12:29:56 -0800

    CORS allow all origins

--- api/settings/dev.py
+++ api/settings/dev.py
@@ -1,4 +1,4 @@
-import re
+# import re
 
 import dj_database_url
 
@@ -16,15 +16,16 @@ DATABASES = {
 }
 
 # CORS
-CORS_ALLOWED_ORIGINS = []
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    # artade projects - match subdomains + root domain
-    re.compile(r"^https://([\w-]+\.)?0xartcade\.xyz$"),
-    # Allow localhost with any port.
-    re.compile(r"^http://localhost:\d+$"),
-    # Allow vercel preview links but only for our app.
-    re.compile(r"^https://([\w-]+)-artcade.vercel\.app$"),
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = []
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     # artade projects - match subdomains + root domain
+#     re.compile(r"^https://([\w-]+\.)?0xartcade\.xyz$"),
+#     # Allow localhost with any port.
+#     re.compile(r"^http://localhost:\d+$"),
+#     # Allow vercel preview links but only for our app.
+#     re.compile(r"^https://([\w-]+)-artcade.vercel\.app$"),
+# ]
 
 # CSRF
 CSRF_TRUSTED_ORIGINS = [
